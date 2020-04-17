156cf8feb291150e30bf0bff36b82ad3d67b0937
Bas van den Eertwegh <b.vandeneertwegh@student.fontys.nl>
2020-04-17 10:10:33 +0200

    cors fix

--- Kwetter/OcelotAPIGateway/Startup.cs
+++ Kwetter/OcelotAPIGateway/Startup.cs
@@ -51,6 +51,7 @@ namespace OcelotAPIGateway
                         ValidateAudience = false
                     };
                 });
+            services.AddCors();
             services.AddOcelot(Configuration);
         }
 
@@ -65,6 +66,7 @@ namespace OcelotAPIGateway
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyHeader());
 
             app.UseAuthorization();
           
