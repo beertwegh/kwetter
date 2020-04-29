cbebdcd6ea2d04eb97152d3fbfdff8711556b48e
Bas van den Eertwegh <b.vandeneertwegh@student.fontys.nl>
2020-04-29 09:48:19 +0200

    fixes

--- Kwetter/AuthService/Models/AuthUser.cs
+++ Kwetter/AuthService/Models/AuthUser.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AuthService.Models
 {
     public class AuthUser
     {
-        [Key]
-        public int UserId { get; set; }
+
+        public Guid UserId { get; set; }
         [Required]
-        
         public string Username { get; set; }
         [Required]
         public string Password { get; set; }
